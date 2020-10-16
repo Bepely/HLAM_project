600818fedb15bd15fa694891f28c8f1f1c84688a
Bepely <63716755+Bepely@users.noreply.github.com>
2020-10-16 12:57:40 +0300

    More routes added in router (artistPage and mainPage

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-const router = require('./routes/router');
+const route = require('./routes/router')
 
 let port = process.env.PORT || 3003;
 
-
-app.get('/',(req, res) => {
-    res.send('SHALOM4IK')
-})
+app.use('/fuck', route);
 
 app.listen(port, ()=>{
     console.log(`CEPBEP 3ATTCK 70T0B0. TT0PT ${port}`);
--- routes/router.js
+++ routes/router.js
@@ -1,1 +1,12 @@
-module.exports();
+var express = require('express');
+var router = express.Router();
+
+console.log('sallllaam');
+
+router.get('/', (req, res)=>{
+    res.send('SALAM')});
+  
+    router.post('/brat', (req, res)=>{
+        res.send('BRATUHA')});
+      
+module.exports = router;;
