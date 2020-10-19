c7107dd8849677d954c29240a2c9479dc943a062
Bepely <63716755+Bepely@users.noreply.github.com>
2020-10-19 20:36:57 +0300

    added graphQL, express-GraphQL

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const route = require('./routes/router.js')
-
+//17.10.2020 
+//DOBAV' BAZU DANNbIX FAUNA ( FaunaDB )!
 let port = process.env.PORT || 3003;
 
+
+
+//GRAPHQL SECTOR
+
+
+//!TESTING!TESTING!TESTING!
 app.use(route);
 
 app.listen(port, ()=>{
