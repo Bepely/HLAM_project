f2647a5c4c944ff91da898eaea80c4f05f9979f6
Bepely <63716755+Bepely@users.noreply.github.com>
2020-10-21 15:13:33 +0300

    Still learning GraphQL. Added fool test schema

--- schema/schema.js
+++ schema/schema.js
@@ -5,38 +5,91 @@ const _= require('lodash');
 
 //dummy data
 let posts = [
-    {name:"Загвоздка", type:"Скетч", id:"1"},
-    {name:"Бич Божий", type:"Скетч", id:"2"},
-    {name:"Holy Water", type:"Трек", id:"3"},
-    {name:"Сволочь", type:"Перформанс", id:"4"},
-    {name:"Тестинг", type:"Тикток", id:"5"},
+    {name:"Загвоздка", type:"Скетч", id:"1", artistid: "3"},
+    {name:"Бич Божий", type:"Скетч", id:"2", artistid: "1"},
+    {name:"Holy Water", type:"Трек", id:"3", artistid: "2"},
+    {name:"Сволочь", type:"Перформанс", id:"4", artistid: "2"},
+    {name:"Тестинг", type:"Тикток", id:"5", artistid: "1"},
+]
+let artists = [
+    {name:"Bepely", age:"22", id:"1"},
+    {name:"Sema", age:"26", id:"2"},
+    {name:"Petr", age:"23", id:"3"}
 ]
 const {
     GraphQLObjectType,
     GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 const PostType = new GraphQLObjectType({
     name: 'Post',
     fields: ()=>({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
-        type: {type: GraphQLString}
+        type: {type: GraphQLString},
+        artist: {
+            type: ArtistType,
+            resolve(parent, args){
+                return _.find(artists, {id: parent.artistid})
+            }
+        }
     })
 });
+
+const ArtistType = new GraphQLObjectType({
+    name: 'Artist',
+    fields: ()=>({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        posts: {
+            type: new GraphQLList(PostType),
+            resolve(parent, args){
+                return _.filter(posts, {artistid: parent.id});
+            }
+        }
+    })
+});
+
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:{
         post:{
             type: PostType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args){
 
            return _.find(posts, {id: args.id});
             
         }
-        }
+        },
+        artist:{
+            type: ArtistType,
+            args: {id:{type:GraphQLID}},
+
+            resolve(parent, args){
+                return _.find(artists, {id: args.id});
+            }
+            },
+            posts: {
+                type: new GraphQLList(PostType),
+                resolve(parent, args){
+                    return posts
+                }
+            },
+            artists: {
+                type: new GraphQLList(ArtistType),
+                
+                resolve(parent, args){
+                    return artists
+                }
+            }
+
     }
     })
 
